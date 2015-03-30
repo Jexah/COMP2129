fe197507a088f846af44134bdc9b5d2e9d80dfea
= <ijexah@gmail.com>
2015-03-30 16:33:04 +1100

    Changed some stuff.

--- reverse_array/reverse_array.c
+++ reverse_array/reverse_array.c
@@ -15,24 +15,23 @@ void die(void)
 
 array_t *createArray(void)
 {
-	array_t newArray;
-	newArray.length = 0;
-	newArray.capacity = 4;
-	newArray.head = calloc(sizeof(int), newArray.capacity);
-	if(newArray.head == NULL) die();
-	return &newArray;
+	array_t *newArray = calloc(sizeof(array_t), 1);
+	newArray->length = 0;
+	newArray->capacity = 4;
+	newArray->head = calloc(sizeof(int), newArray.capacity);
+	if(newArray->head == NULL) die();
+	return newArray;
 }
 
-int appendNumber(int x, array_t **target)
+int appendNumber(int x, array_t *target)
 {
-	if(((array_t *)*target)->length == ((array_t *)*target)->capacity)
+	if(target->length == target->capacity)
 	{
-		array_t *newArray = realloc((array_t *)*target, sizeof(int) * ((array_t *)*target)->capacity * 2);
-		if(newArray == NULL) return 0;
-		*target = newArray;
-		((array_t *)*target)->capacity = ((array_t *)*target)->capacity * 2;
+		void *temp = realloc(target->head, sizeof(int) * target->capacity * 2);
+		if(temp == NULL) return 0;
+		target->head = temp;
 	}
-	((array_t *)*target)->head[((array_t *)*target)->length++] = x;
+	target->head[length++] = x;
 	return 1;
 }
 
