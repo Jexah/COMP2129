58d4040ce05bdd38f7512e43649d59b03d116e4d
= <ijexah@gmail.com>
2015-03-30 17:11:07 +1100

    Started linked list

---
+++ linked_list/linked_list.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node Node;
+
+struct Node
+{
+	Node *next;
+	int value;
+}
+
+int prependToList(Node *list, Node *node)
+{
+	node->next = list;
+	list = node;
+}
+
+Node *createNode(void)
+{
+	Node *temp = calloc(sizeof(Node), 1);
+	temp->next = NULL;
+	temp->value = 0;
+	return temp;
+}
+
+int main(void)
+{
+	Node *list = createNode();
+
+	Node *node = createNode();
+
+	node->value = 5;
+
+	prependToList(list, node);
+
+	printf("%d\n", list->value);
+
+	return 0;
+}
