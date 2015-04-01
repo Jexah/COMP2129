ed556434d00c316f35547ce55e09cb096ad29132
= <ijexah@gmail.com>
2015-04-01 19:49:07 +1100

    .

--- challenges/mystery_box/mystery_box.c
+++ challenges/mystery_box/mystery_box.c
@@ -6,7 +6,45 @@ int main(void)
 	int width;
 	int height;
 	printf("Enter the size of the box: ");
-	printf("success: %d\n", scanf("%2d %2d", &width, &height));
-	printf("%dx %dy", width, height);
+	if(scanf("%2d %2d", &width, &height) == 2 && width > 0 && width < 21 && height > 0 && height < 21)
+	{
+		for(int y = 0; y < height; ++y)
+		{
+			for(int x = 0; x < width; ++x)
+			{
+				if(y == 0 || y == height-1)
+				{
+					if(x == 0 || x == width-1))
+					{
+						printf("+");
+						if(x == width-1)
+						{
+							printf("\n");
+						}
+					}
+					else
+					{
+						printf("-");
+					}
+				}
+				else
+				{
+					if(x == 0 || x == width-1)
+					{
+						printf("|");
+						if(x == width-1)
+						{
+							printf("\n");
+						}
+					}
+					else
+					{
+						printf(" ");
+					}
+				}
+			}
+		}
+	}
+	printf("Invalid input");
 	return 0;
 }
