5231d128a3b374ff09ff2bcb101682652385e19e
= <ijexah@gmail.com>
2015-04-01 19:57:39 +1100

    .

--- challenges/mystery_box/mystery_box.c
+++ challenges/mystery_box/mystery_box.c
@@ -6,7 +6,7 @@ int main(void)
 	int width;
 	int height;
 	printf("Enter the size of the box: ");
-	if(scanf("%2d %2d", &width, &height) == 2 && width > 0 && width < 21 && height > 0 && height < 21)
+	if(scanf("%2d%2d", &width, &height) == 2 && width > 0 && width < 21 && height > 0 && height < 21)
 	{
 		for(int y = 0; y < height; ++y)
 		{
@@ -17,10 +17,6 @@ int main(void)
 					if(x == 0 || x == width-1)
 					{
 						printf("+");
-						if(x == width-1)
-						{
-							printf("\n");
-						}
 					}
 					else
 					{
